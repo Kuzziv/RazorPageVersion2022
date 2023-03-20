b7014a63c01088a3890d209cc162d4826526eef5
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2023-03-20 23:05:16 +0100

    Item update

--- Service/SQLQueryService/SQLQueryItem.cs
+++ Service/SQLQueryService/SQLQueryItem.cs
@@ -62,6 +62,25 @@ namespace RazorPageVersion2022.Service.SQLQueryService
 
         public Item GetItemById(int id)
         {
+           
+            string query = $"Select * from Item where id = {id}";
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Item item = new Item();
+                        item.Id = Convert.ToInt32(reader[0]);
+                        item.Name = Convert.ToString(reader[1]);
+                        item.Price = Convert.ToInt32(reader[2]);
+                        return item; 
+                    }
+                }
+            }
             return null;
         }
 
